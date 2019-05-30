6efeba47ae08c3abe671caf2b9159bcda0bd80dd
Brayden-Osborne <36768921+Brayden-Osborne@users.noreply.github.com>
2019-05-30 18:35:18 -0400

    Removed .idea folder

---
+++ Source/python/test.py
@@ -0,0 +1,63 @@
+from sklearn import model_selection, preprocessing
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+import pandas, numpy
+
+testinput = [[1, 'December 19 2018 Purchase of "Last Battleground : Mech" on App Store, '
+                 'Invoice available on this email, Thankyou for shopping with apple.. '],
+             [0, 'Hi, I am a good email'],
+             [0, 'Hi, I am another good email'],
+             [1, 'Hi I am a bad email']
+             ]
+
+
+labels, texts = [], []
+for i, line in enumerate(testinput):
+    content = line
+    labels.append(content[0])
+    texts.append(" ".join(content[1:]))
+
+# create a dataframe using texts and lables
+trainDF = pandas.DataFrame()
+trainDF['text'] = texts
+trainDF['label'] = labels
+
+# split the dataset into training and validation datasets
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'])
+
+# label encode the target variable
+encoder = preprocessing.LabelEncoder()
+train_y = encoder.fit_transform(train_y)
+valid_y = encoder.fit_transform(valid_y)
+
+
+# create a count vectorizer object
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(trainDF['text'])
+
+# transform the training and validation data using count vectorizer object
+xtrain_count = count_vect.transform(train_x)
+xvalid_count = count_vect.transform(valid_x)
+
+
+# word level tf-idf
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
+tfidf_vect.fit(trainDF['text'])
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
+
+# ngram level tf-idf
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram.fit(trainDF['text'])
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+
+# characters level tf-idf
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram_chars.fit(trainDF['text'])
+xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x)
+xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x)
+
+
+
+x=1
