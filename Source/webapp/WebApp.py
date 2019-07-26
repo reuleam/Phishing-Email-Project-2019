2fbd15d6b9441b2fbeace2ded8a432886cc143a5
reuleam <austinreule@gmail.com>
2019-07-26 10:37:38 -0400

    WebApp changes

---
+++ Source/webapp/WebApp.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+from PhishDetector import ThreatDetector
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    subject_content = request.form['subject']
+    body_content = request.form["body"]
+
+    detector = ThreatDetector()
+    detector.detect_subject(subject_content)
+    detector.detect_body(body_content)
+    subject_threats, body_threats = detector.return_threats()
+    subject_chance, body_chance = detector.return_stats()
+
+    if len(subject_threats) == 0:
+        sub_return = """There were no words in your subject line that we commonly found in phishing scams. 
+                        This does not guarantee that it is not a phishing email. Please see the \"Resources\" 
+                        for additional materials to assist in identifying threats"""
+    else:
+        sub_return = """There were one or more words found in your subject line that are commonly found in
+                        phishing emails:"""
+        sub_threat_words = ','.join([word + '\n'for word in subject_threats])
+    if len(body_threats) == 0:
+        bod_return = """There were no words in your email body that we commonly found in phishing scams. 
+                        This does not guarantee that it is not a phishing email. Please see the \"Resources\" 
+                        for additional materials to assist in identifying threats"""
+    else:
+        bod_return = """There were one or more words found in your email body that are commonly found in 
+                        phishing emails:""" 
+        bod_threat_words = ','.join([word + '\n'for word in body_threats])
+
+    return render_template('output.html',sub_return=sub_return, sub_threat_words=sub_threat_words, bod_threat_words= bod_threat_words, bod_return=bod_return)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
